312fe10e44cd688e675c9053e09381f09ab5d45e
hh120901 <nguyenvanhau1209@gmail.com>
2021-09-08 14:02:47 +0700

    Th1
    test

---
+++ SnakeGame/src/snakeGame/SnakeGame.java
@@ -0,0 +1,9 @@
+package snakeGame;
+import java.awt.*;
+import javax.swing.*;
+
+public class SnakeGame {
+	public static void main(String[] args) {
+		new SnakeFrame();
+	}
+}
---
+++ X_Ogame/src/gameXO/main.java
@@ -0,0 +1,10 @@
+package gameXO;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TicTacToe tictactoe = new TicTacToe();
+	}
+
+}
